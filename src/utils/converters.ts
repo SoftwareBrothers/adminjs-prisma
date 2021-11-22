f9e790416cbc26e3d76095465015a0b4f3e380b9
Leonardo Meneguzzi <meneguzzileo@gmail.com>
2021-11-22 03:26:49 -0300

    fix: add uuid check on filter
    If the database is postgres and the column type is UUID, the "String contains" from prisma cause an exception

--- src/utils/converters.ts
+++ src/utils/converters.ts
@@ -32,6 +32,7 @@ export const convertParam = (
 export const convertFilter = (modelFields: DMMF.Model['fields'], filterObject?: Filter): Record<string, any> => {
   if (!filterObject) return {};
 
+  const uuidRegex = /^[0-9A-F]{8}-[0-9A-F]{4}-[5|4|3|2|1][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
   const { filters = {} } = filterObject;
   return Object.entries(filters).reduce((where, [name, filter]) => {
     if (['boolean', 'number', 'float', 'object', 'array'].includes(filter.property.type())) {
@@ -45,6 +46,8 @@ export const convertFilter = (modelFields: DMMF.Model['fields'], filterObject?: 
         where[name] = { lte: new Date(filter.value.to) };
       }
     } else if ((filter.property as Property).isEnum()) {
+      where[name] = { equals: filter.value };
+    } else if (filter.property.type() === 'string' && uuidRegex.test(filter.value.toString())) {
       where[name] = { equals: filter.value };
     } else if (filter.property.type() === 'reference' && (filter.property as Property).foreignColumnName()) {
       where[(filter.property as Property).foreignColumnName() as string] = convertParam(
