459f66fafaaec463b8b02166ab0dc245620aec07
Roman Vašek <rvasek0@gmail.com>
2023-12-22 15:27:31 +0100

    feat: converting condition for exact-string PropertyType

--- src/utils/converters.ts
+++ src/utils/converters.ts
@@ -17,7 +17,9 @@ export const convertParam = (
     return safeParseNumber(value);
   }
   if (type === 'reference') {
-    const foreignColumn = fields.find((field) => field.name === property.foreignColumnName());
+    const foreignColumn = fields.find(
+      (field) => field.name === property.foreignColumnName(),
+    );
     if (!foreignColumn) return value;
     if (value === undefined || value === null) return value;
 
@@ -30,17 +32,31 @@ export const convertParam = (
   return value;
 };
 
-export const convertFilter = (modelFields: DMMF.Model['fields'], filterObject?: Filter): Record<string, any> => {
+export const convertFilter = (
+  modelFields: DMMF.Model['fields'],
+  filterObject?: Filter,
+): Record<string, any> => {
   if (!filterObject) return {};
 
   const uuidRegex = /^[0-9A-F]{8}-[0-9A-F]{4}-[5|4|3|2|1][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
   const { filters = {} } = filterObject;
   return Object.entries(filters).reduce((where, [name, filter]) => {
-    if (['boolean', 'number', 'float', 'object', 'array'].includes(filter.property.type())) {
+    if (
+      ['boolean', 'number', 'float', 'object', 'array'].includes(
+        filter.property.type(),
+      )
+    ) {
       where[name] = safeParseJSON(filter.value as string);
     } else if (['date', 'datetime'].includes(filter.property.type())) {
-      if (typeof filter.value !== 'string' && filter.value.from && filter.value.to) {
-        where[name] = { gte: new Date(filter.value.from), lte: new Date(filter.value.to) };
+      if (
+        typeof filter.value !== 'string'
+        && filter.value.from
+        && filter.value.to
+      ) {
+        where[name] = {
+          gte: new Date(filter.value.from),
+          lte: new Date(filter.value.to),
+        };
       } else if (typeof filter.value !== 'string' && filter.value.from) {
         where[name] = { gte: new Date(filter.value.from) };
       } else if (typeof filter.value !== 'string' && filter.value.to) {
@@ -48,14 +64,18 @@ export const convertFilter = (modelFields: DMMF.Model['fields'], filterObject?: 
       }
     } else if ((filter.property as Property).isEnum()) {
       where[name] = { equals: filter.value };
-    } else if (filter.property.type() === 'string' && uuidRegex.test(filter.value.toString())) {
+    } else if (
+      filter.property.type() === 'string'
+      && uuidRegex.test(filter.value.toString())
+    ) {
       where[name] = { equals: filter.value };
-    } else if (filter.property.type() === 'reference' && (filter.property as Property).foreignColumnName()) {
-      where[(filter.property as Property).foreignColumnName() as string] = convertParam(
-        filter.property as Property,
-        modelFields,
-        filter.value,
-      );
+    } else if (
+      filter.property.type() === 'reference'
+      && (filter.property as Property).foreignColumnName()
+    ) {
+      where[(filter.property as Property).foreignColumnName() as string] = convertParam(filter.property as Property, modelFields, filter.value);
+    } else if (filter.property.type() === 'exact-string') {
+      where[name] = { equals: filter.value.toString() };
     } else {
       where[name] = { contains: filter.value.toString() };
     }
